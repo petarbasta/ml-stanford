5370d7d6a0737ec32162f5e4071bf4993f0438e3
Petar Basta <petar.basta@hotmail.com>
2019-03-04 00:24:48 +0800

    Finished Assignment 4

--- machine-learning-ex4/ex4/nnCostFunction.m
+++ machine-learning-ex4/ex4/nnCostFunction.m
@@ -62,25 +62,82 @@ Theta2_grad = zeros(size(Theta2));
 %               and Theta2_grad from Part 2.
 %
 
+%calculate outputs
+X = [ones(m,1) X]; % 5000*401
+a1 = X;
+z2 = Theta1 * a1'; % (25*401)*(401*5000)
+a2 = sigmoid(z2); % (25*5000)
 
+a2 = [ones(m,1) a2'];
+z3 = Theta2 * a2';
+a3 = sigmoid(z3); 
 
+newY = zeros(num_labels, m); % 10*5000
+for i=1:m
+  newY(y(i),i)=1;
+end
 
+J = (1/m) * sum ( sum ( (-newY) .* log(a3) - (1-newY) .* log(1-a3) ));
 
+% Note we should not regularize the terms that correspond to the bias. 
+% For the matrices Theta1 and Theta2, this corresponds to the first column of each matrix.
 
+% Regularization
+reg = lambda  * (sum( sum ( Theta1(:,2:end) .^ 2 )) + sum( sum ( Theta2(:,2:end).^ 2 ))) / (2*m);
 
+% Regularized cost function
+J = J + reg;
 
+% Part 2: Implement the backpropagation algorithm to compute the gradients
+%         Theta1_grad and Theta2_grad. You should return the partial derivatives of
+%         the cost function with respect to Theta1 and Theta2 in Theta1_grad and
+%         Theta2_grad, respectively. After implementing Part 2, you can check
+%         that your implementation is correct by running checkNNGradients
+%
+%         Note: The vector y passed into the function is a vector of labels
+%               containing values from 1..K. You need to map this vector into a 
+%               binary vector of 1's and 0's to be used with the neural network
+%               cost function.
+%
+%         Hint: We recommend implementing backpropagation using a for-loop
+%               over the training examples if you are implementing it for the 
+%               first time.
 
+% Back propagation
+for t=1:m
 
+    % Step 1
+	a1 = X(t,:); 
+    a1 = a1'; % (401*1)
+	z2 = Theta1 * a1; % (25*401)*(401*1)
+	a2 = sigmoid(z2); % (25*1)
+    
+    a2 = [1 ; a2]; % adding a bias (26*1)
+	z3 = Theta2 * a2; % (10*26)*(26*1)
+	a3 = sigmoid(z3); % final activation layer a3 == h(theta) (10*1)
+    
+    % Step 2
+	delta_3 = a3 - newY(:,t); % (10*1)
+	
+    z2=[1; z2]; % bias (26*1)
+    % Step 3
+    delta_2 = (Theta2' * delta_3) .* sigmoidGradient(z2); % ((26*10)*(10*1))=(26*1)
 
+    % Step 4
+	delta_2 = delta_2(2:end); % skipping sigma2(0) (25*1)
 
+	Theta2_grad = Theta2_grad + delta_3 * a2'; % (10*1)*(1*26)
+	Theta1_grad = Theta1_grad + delta_2 * a1'; % (25*1)*(1*401)
+    
+end;
 
+% Step 5
+Theta2_grad = (1/m) * Theta2_grad; % (10*26)
+Theta1_grad = (1/m) * Theta1_grad; % (25*401)
 
+Theta2_grad(:, 2:end) = Theta2_grad(:, 2:end) + lambda/m * Theta2(:, 2:end);
+Theta1_grad(:, 2:end) = Theta1_grad(:, 2:end) + lambda/m * Theta1(:, 2:end);
 
-
-
-
-
-% -------------------------------------------------------------
 
 % =========================================================================
 
